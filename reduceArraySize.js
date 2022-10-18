ae067a97ab5e42de4b821563befbc5aa5e5c026b
Senait Alemayehu <senaitalemayehu27@gmail.com>
2022-10-18 22:09:55 +0300

    reduce array size

---
+++ reduceArraySize.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var minSetSize = function(arr) {
+        let obj = {},
+    count = 0;
+
+    for (let num of arr) {
+      num in obj ? obj[num] += 1 : obj[num] = 1;
+    }
+
+    let newArr = Object.values(obj),
+    half = arr.length >> 1,
+    sum = 0,
+    i = 0;
+
+    newArr.sort((a, b) => b - a);
+
+    while (sum < half) {
+      sum += newArr[i++];
+      count++;
+    }
+
+    return count;
+    
+};
